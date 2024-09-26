72b3ad290a0dcc938e2bd9e01a422dd9df8c75c1
unknown <ashik@example.com>
2024-09-27 00:34:51 +0530

    some changes in env file

--- src/middleware/auth0.ts
+++ src/middleware/auth0.ts
@@ -14,8 +14,8 @@ declare global {
 }
 
 export const jwtCheck = auth({
-    audience:'mern-food-ordering-app-api',
-    issuerBaseURL:'https://dev-f3yhe4wrb52d8q5s.us.auth0.com/',
+    audience:process.env.AUTH0_AUDIENCE,
+    issuerBaseURL:process.env.AUTH0_ISSUER_BASE_URL,
     tokenSigningAlg: 'RS256'
   });
 
